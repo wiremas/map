709b25726b97f2f5a919680fb86be52a6787aef4
anno <anno_schachner@gmx.at>
2018-07-10 15:14:28 +0200

    add runtime globals to map

--- services/map_async.py
+++ services/map_async.py
@@ -6,28 +6,31 @@ import importlib
 import multiprocessing
 
 if __name__ == '__main__':
-
     # decode and unpickle function and input list
     code = dill.loads(base64.decodestring(sys.argv[1]))
     iterable = dill.loads(base64.decodestring(sys.argv[2]))
     chunk_size = dill.loads(base64.decodestring(sys.argv[3]))
     modules = dill.loads(base64.decodestring(sys.argv[4]))
-    #  service = dill.loads(base64.decodestring(sys.argv[5]))
+    runtime_globals = dill.loads(base64.decodestring(sys.argv[5]))
 
     # run import & generate globals for the function
-    runtime_globals = {}
+    #  runtime_globals = {}
     for module in modules:
         module = module.split(' as ')
         if len(module) == 2:
             name = module[1]
             module = importlib.import_module(module[0])
-            runtime_globals[name] = module
+            globals()[name] = module
         else:
             module = importlib.import_module(module[0])
-            runtime_globals[module.__name__] = module
+            globals()[module.__name__] = module
+
+    # add runtime globals
+    for glb in runtime_globals:
+        globals()[glb.__name__] = glb
 
     # create funcrtion from code object
-    func = types.FunctionType(code, runtime_globals, 'remote_func')
+    func = types.FunctionType(code, globals(), 'remote_func')
 
     # create a hook that can be pickled by Pool's Queue
     def hook(args):
